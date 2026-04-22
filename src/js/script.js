458c11ff8c8262c4e31936d2e7416514a5e17559
Firaxel <firaxel84@gmail.com>
2026-04-22 18:10:53 +0300

    works block finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,39 @@
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import "/src/sass/style.scss";
+
+// init Swiper:
+try {
+    const swiper = new Swiper('.works__slider', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        navigation: {
+            nextEl: '.icon-right-open',
+            prevEl: '.icon-left-open',
+        },
+
+        breakpoints: {
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            1920: {
+                slidesPerView: 3,
+                spaceBetween: 35,
+            },
+        },
+
+        modules: [Navigation, Pagination],
+    });
+} catch (e) { };
